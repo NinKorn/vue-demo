7e29a348d53c8ef2c04e5c4b93ad18779eb0f7fc
NinKorn <15377619772@163.com>
2019-02-12 18:51:50 +0800

    脚手架构建项目

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- 头部 -->
+    <mt-header fixed title="Vue商城项目"></mt-header>
+    <transition>
+      <router-view/>
+    </transition>
+    <!-- 底部 -->
+    <nav class="mui-bar mui-bar-tab">
+      <a href="home.html" class="mui-tab-item mui-active" id="defaultTab">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a href="message.html" class="mui-tab-item">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">商城</span>
+      </a>
+      <a href="setting.html" class="mui-tab-item">
+        <span class="mui-icon mui-icon-home">
+          <span class="mui-badge">5</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+  padding-top: 40px;
+}
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//引入样式 清除默认样式
+import './style/vue.css'
+//引入UI库
+import './lib/mui/fonts/mui-icons-extra.ttf'
+import './lib/mui/fonts/mui.ttf'
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
+import MintUI from 'mint-ui' //把所有的组件都导入进来
+// 这里 可以省略 node_modules 这一层目录
+import 'mint-ui/lib/style.css'
+// 将 MintUI 安装到 Vue 中
+Vue.use(MintUI) // 把所有的组件，注册为全局的组件
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
